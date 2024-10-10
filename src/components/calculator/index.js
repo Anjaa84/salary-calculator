b281700b5f98f041dfa67df0465d062a19528430
Anjanan <anjananmy@gmail.com>
2024-10-10 08:17:14 +0530

    Weekly added

--- src/components/calculator/index.js
+++ src/components/calculator/index.js
@@ -27,6 +27,7 @@ const LoanCalculator = () => {
     (parsedLeaseAmount + parsedOtherCharges);
   const agreedAmount = parsedLeaseAmount + parsedOtherCharges + totalInterest;
   const rental = agreedAmount / parsedDuration;
+  const weeklyRental = rental / 4;
 
   // Document Charges calculations
   const documentChargesAmount =
@@ -58,7 +59,7 @@ const LoanCalculator = () => {
         </Col>
         <Col>
           <Title level={2} style={{ margin: "0 0 0 20px" }}>
-            Monik Homes CAL Calculator
+            Loan Calculator Monik / CMC
           </Title>
         </Col>
       </Row>
@@ -115,7 +116,12 @@ const LoanCalculator = () => {
       <Title level={3}>Results</Title>
       <p>Total Interest: {totalInterest.toFixed(2)}</p>
       <p>Agreed Amount: {agreedAmount.toFixed(2)}</p>
-      <p>Rental (Per Month): {rental.toFixed(2)}</p>
+      <p style={{ fontWeight: "bold", color: "#00008B" }}>
+        Rental (Per Week): {weeklyRental.toFixed(2)}
+      </p>
+      <p style={{ fontWeight: "bold", color: "#00008B" }}>
+        Rental (Per Month): {rental.toFixed(2)}
+      </p>
       <p>Document Charges Amount: {documentChargesAmount.toFixed(2)}</p>
       <p>Release Amount: {releaseAmount.toFixed(2)}</p>
 
